903ce9284b9e75edef589bd34646b4cabf89a287
maachouO <24957623+Maaochou@users.noreply.github.com>
2025-08-26 10:22:08 +0200

    chore: reformat lines

--- src/main/kotlin/coffeemachine/DrinkType.kt
+++ src/main/kotlin/coffeemachine/DrinkType.kt
@@ -3,15 +3,16 @@ package coffeemachine
 import java.math.BigDecimal
 
 enum class DrinkType(
-    val value: String,
-    val price: BigDecimal = BigDecimal.ZERO,
-    val canBeSoldExtraHot: Boolean = false
+    val value: String, val price: BigDecimal = BigDecimal.ZERO, val canBeSoldExtraHot: Boolean = false
 ) {
-    COFFEE("C", 0.6.toBigDecimal(), true), TEA("T", 0.4.toBigDecimal(), true), CHOCOLATE(
-        "H",
-        0.5.toBigDecimal(),
-        true
+    COFFEE("C", 0.6.toBigDecimal(), true),
+
+    TEA("T", 0.4.toBigDecimal(), true),
+
+    CHOCOLATE(
+        "H", 0.5.toBigDecimal(), true
     ),
+
     ORANGE_JUICE(
         "O", 0.6.toBigDecimal(), false
     )
